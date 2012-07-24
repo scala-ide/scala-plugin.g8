4aa7ed1578cde7e3c3d81c5114c00128ac67e186
Luc Bourlier <luc.bourlier@typesafe.com>
2012-07-24 19:01:30 +0200

    Some typos

--- src/main/g8/$pluginName$/src/$pluginName__packaged$/SomeInterestingCode.scala
+++ src/main/g8/$pluginName$/src/$pluginName__packaged$/SomeInterestingCode.scala
@@ -1,11 +1,9 @@
 package $pluginName$
 
-import scala.tools.eclipse.javaelements.ScalaCompilationUnit
-
 object SomeInterestingCode {
   
-  def div(a: Int, b: Int): Option[Int] = {                          
-    if (b == 0) {                                                   
+  def div(a: Int, b: Int): Option[Int] = {
+    if (b == 0) {
       None
     } else {
       Some(a / b)
